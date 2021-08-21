095ead9d95110fde242675526721a110d921103f
Siddharth <sid.auburn@gmail.com>
2021-08-21 17:20:01 -0400

    modified expense component to show the date in correct format

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem />
+      <ExpenseItem title="Toilet Paper" date={new Date(2021, 8, 15)} amount="25" />
+      <ExpenseItem title="Sugar" date={new Date(2021, 7, 20)} amount="10" />
+      <ExpenseItem title="Salt" date={new Date(2021, 8, 10)} amount="5" />
+      <ExpenseItem title="Cookies" date={new Date(2021, 6, 25 )} amount="8" />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,25 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+
+function ExpenseItem(props) {
+    
+    // date config object to store correct formated date from props.
+    const dateConfig = {
+        month: props.date.toLocaleString('en-US', {month: 'long'}),
+        day: props.date.toLocaleString('en-US', {day: '2-digit'}),
+        year: props.date.getFullYear()
+    }
+
     return (
         <div className="expense-item">
-            <div>Date</div>
+            <div>
+                <div>{dateConfig.month}</div>
+                <div>{dateConfig.year}</div>
+                <div>{dateConfig.day}</div>
+            </div>  
             <div className="expense-item__description">
-                <h2>Title</h2>
-                <div className="expense-item__price">Amount</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
     )
