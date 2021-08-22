67b6360ca56834c6bda7781b049aa1482026bedd
Siddharth <sid.auburn@gmail.com>
2021-08-21 21:45:55 -0400

    organize project components in separate folder

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import ExpenseItem from "./components/ExpenseItem";
+import React from 'react'
+import Expenses from './components/expenses/Expenses'
 
 
 function App() {
+
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem title="Toilet Paper" date={new Date(2021, 8, 15)} amount="25" />
-      <ExpenseItem title="Sugar" date={new Date(2021, 7, 20)} amount="10" />
-      <ExpenseItem title="Salt" date={new Date(2021, 8, 10)} amount="5" />
-      <ExpenseItem title="Cookies" date={new Date(2021, 6, 25 )} amount="8" />
+      <Expenses items={expenses} />
     </div>
   );
 }
+
 
 export default App;
