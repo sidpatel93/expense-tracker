e8f52feec28c8c020789669e49d4cc80d7d211ed
Siddharth <sid.auburn@gmail.com>
2021-08-21 20:01:33 -0400

    separated date component and added css to it.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,22 +1,13 @@
 import './ExpenseItem.css';
-
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
     
-    // date config object to store correct formated date from props.
-    const dateConfig = {
-        month: props.date.toLocaleString('en-US', {month: 'long'}),
-        day: props.date.toLocaleString('en-US', {day: '2-digit'}),
-        year: props.date.getFullYear()
-    }
+    
 
     return (
         <div className="expense-item">
-            <div>
-                <div>{dateConfig.month}</div>
-                <div>{dateConfig.year}</div>
-                <div>{dateConfig.day}</div>
-            </div>  
+             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
